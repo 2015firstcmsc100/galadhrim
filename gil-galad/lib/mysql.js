fbbe992466717c3b9fc2c4c48a19d784086a1e20
Rommel Bulalacao <mightybulacs@gmail.com>
2015-08-24 17:10:35 +0800

    added logger within mysql lib

--- gil-galad/lib/mysql.js
+++ gil-galad/lib/mysql.js
@@ -1,5 +1,8 @@
 var mysql = require('mysql'),
+	logger = require(__dirname + '/../../lib/logger'),
 	config = require(__dirname + '/../../config/config').db;
+
+	logger.log('info', 'Connecting to database at ' + config.host);
 
 	db = mysql.createConnection({
 		host     : config.host,
