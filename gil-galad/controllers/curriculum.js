4d36770ab7744add6b7fbb3a7eb408518e14ef0c
jaredespineli <jaredespineli@gmail.com>
2015-11-30 22:21:23 +0800

    POST for curriculum done and returns error 451 if any parameter is missing, also added curriculum file in test

--- gil-galad/controllers/curriculum.js
+++ gil-galad/controllers/curriculum.js
@@ -1,9 +1,9 @@
-var	logger = require(__dirname + '/../../lib/logger'),
+var logger = require(__dirname + '/../../lib/logger'),
     db = require(__dirname + '/../lib/mysql');
 
 
 exports.find = function(req, res, next) {
-	db.query("SELECT * FROM degree_program", function(err, rows) {
+	db.query("SELECT * FROM curriculum", function(err, rows) {
 		if (err) return next(err);
 		res.send(rows);
 	});
@@ -11,16 +11,15 @@ exports.find = function(req, res, next) {
 
 
 exports.findOne = function(req, res, next) {
-	db.query("SELECT * FROM degree_program WHERE id=?", [req.params.id], function(err, rows) {
+	db.query("SELECT * FROM curriculum WHERE id=?", [req.params.id], function(err, rows) {
 		if (err) return next(err);
 		if (rows.length === 0) {
-			res.send(404, {message: 'Degree program not found.'});
+			res.send(404, {message: 'Curriculum not found.'});
 		} else {
 			res.send(rows[0]);
 		}
 	});
 };
-
 
 exports.insert = function(req, res, next) {
 	if (!req.body.code) {
@@ -29,12 +28,15 @@ exports.insert = function(req, res, next) {
 	if (!req.body.name) {
 		return res.send(451, {'error': true, 'message': 'Missing parameter: name'});
 	}
+	if (!req.body.degreeProgramId) {
+		return res.send(451, {'error': true, 'message': 'Missing parameter: degreeProgramId'});
+	}
 	
-	db.query("INSERT INTO degree_program(code, name) VALUES(?, ?)", [req.body.code, req.body.name], function(err, row) {
+	db.query("INSERT INTO curriculum(code, name, degreeProgramId) VALUES(?, ?, ?)", [req.body.code, 		req.body.name, req.body.degreeProgramId], function(err, row) {
 		if (err) return next(err);
 		selectOne(row.insertId, function(newRow) {
 			if (!newRow) {
-				res.send(552, {message: 'Degree program ('+row.insertId+') was not created.'});
+				res.send(552, {message: 'Curriculum ('+row.insertId+') was not created.'});
 			} else {
 				res.send(newRow);
 			}
@@ -44,11 +46,11 @@ exports.insert = function(req, res, next) {
 
 
 exports.update = function(req, res, next) {
-	db.query("UPDATE degree_program SET ? WHERE id=?", [req.body, req.params.id], function(err, rows) {
+	db.query("UPDATE curriculum SET ? WHERE _id=?", [req.body, req.params.id], function(err, rows) {
 		if (err) return next(err);
 		selectOne(req.params.id, function(updatedRow) {
 			if (!updatedRow) {
-				res.send(553, {message: 'Degree program ('+req.params.id+') was not updated.'});
+				res.send(553, {message: 'Curriculum ('+req.params.id+') was not updated.'});
 			} else {
 				res.send(updatedRow);
 			}
@@ -58,10 +60,10 @@ exports.update = function(req, res, next) {
 
 
 exports.remove = function(req, res, next) {
-	db.query("DELETE FROM degree_program WHERE id=?", [req.params.id], function(err, row) {
+	db.query("DELETE FROM curriculum WHERE _id=?", [req.params.id], function(err, row) {
 		if (err) return next(err);
 		if (row.affectedRows === 0) {
-			res.send(554, {message: 'Degree program ('+req.params.id+') was not removed.'});
+			res.send(554, {message: 'Curriculum ('+req.params.id+') was not removed.'});
 		} else {
 			res.send(202, row);
 		}
@@ -71,7 +73,7 @@ exports.remove = function(req, res, next) {
 
 
 var selectOne = function(id, callback) {
-	db.query("SELECT * FROM degree_program WHERE id=? LIMIT 1", [id], function(err, rows) {
+	db.query("SELECT * FROM curriculum WHERE _id=? LIMIT 1", [id], function(err, rows) {
 		if (err) return next(err);
 		if (rows.length === 0) {
 			callback(null);
--- schema.sql
+++ schema.sql
@@ -3,3 +3,29 @@ CREATE TABLE degree_program(
 	code VARCHAR(16) UNIQUE,
 	name VARCHAR(128)
 );
+
+DROP TABLE IF EXISTS curriculum;
+CREATE TABLE curriculum(
+    _id int(11) AUTO_INCREMENT PRIMARY KEY,
+    code varchar(64) NOT NULL,
+    name varchar(256) NOT NULL,
+    degreeProgramId int(11) NOT NULL,
+    _created timestamp DEFAULT CURRENT_TIMESTAMP,
+    _recStatus varchar(8) DEFAULT 'ACTIVE',
+    _updated timestamp
+);
+
+
+DROP TABLE IF EXISTS curriculum_course;
+CREATE TABLE curriculum_course(
+    _id int(11) AUTO_INCREMENT PRIMARY KEY,
+    courseId int(11) NOT NULL,
+    curriculumId int(11) NOT NULL,
+    year varchar(16) NOT NULL,
+    semester varchar(16) NOT NULL,
+    prerequisites varchar(512) DEFAULT NULL,
+    _created timestamp DEFAULT CURRENT_TIMESTAMP,
+    _recStatus varchar(8) DEFAULT 'ACTIVE',
+    _updated timestamp
+);
+
