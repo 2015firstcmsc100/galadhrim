d0dc09229a462ef5f94b7b4c0bf3e8b36c04b54f
pcmquizon <camillemendoza17@gmail.com>
2015-12-04 01:48:26 +0800

    added test for create log, modified create section & router.js

--- config/router.js
+++ config/router.js
@@ -1,13 +1,6 @@
 var degreeProgram = require(__dirname + '/../gil-galad/controllers/degree-program');
 var curriculum = require(__dirname + '/../gil-galad/controllers/curriculum');
-var finalize = require(__dirname + '/../gil-galad/controllers/api/ocm/finalize');
-var course = require(__dirname + '/../gil-galad/controllers/course');
-var password = require(__dirname + '/../gil-galad/controllers/api/password/reset_password');
-var planOfStudy = require(__dirname + '/../gil-galad/controllers/api/plan-of-study');
-var grades = require(__dirname + '/../gil-galad/controllers/api/students');
-var employees = require(__dirname + '/../gil-galad/controllers/api/employees');
-var studentRecord = require(__dirname + '/../gil-galad/controllers/api/student-record');
-var assignFaculty = require(__dirname + '/../gil-galad/controllers/api/ocm/assign_faculty');
+var section = require(__dirname + '/../gil-galad/controllers/api/section');
 
 module.exports = function(router, logger) {
 	router.all('*', function (req, res, next) {
@@ -36,37 +29,9 @@ module.exports = function(router, logger) {
 		.put(curriculum.update)
 		.delete(curriculum.remove);
 	
-	router.route('/sections')
-		.get(section.find)
+	router.route('/api/sections')
 		.post(section.insert);
 
-	router.route('/api/ocm/finalize/:id')
-		.put(finalize.update);
-	
-	router.route('/api/password/reset/:id')
-		.get(password.reset);
-
-	router.route('/courses')
-		.get(course.find);
-		
-	router.route('/api/plan-of-study/:id')
-		.put(plan_of_study.update);
-
-	router.route('/api/plan-of-study')
-		.post(planOfStudy.insert);
-
-	router.route('/api/students/:id/grades')
-		.get(grades.findstudentGrade);
-
-	router.route('/api/employees')
-		.get(employees.findEmployees);
-
-	router.route('/api/student-record/:id')
-		.get(studentRecord.findAStudentRecord);
-		
-	router.route('/api/ocm/assign-faculty/:id')
-		.put(assignFaculty.update);
-	
 	router.all('*', function (req, res, next) {
 		res.send(404, {message : 'Nothing to do here.'});
 	});
