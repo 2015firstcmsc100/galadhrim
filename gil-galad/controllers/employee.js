314d5fa286e4a1d9aa40669793120fa75a7c955e
hoommaah <hcmalijan@up.edu.ph>
2015-12-08 13:45:41 +0800

    Employee update

---
+++ gil-galad/controllers/employee.js
@@ -0,0 +1,15 @@
+var	logger = require(__dirname + '/../../lib/logger'),
+    db = require(__dirname + '/../lib/mysql');
+    
+exports.update = function(req, res, next) {
+	db.query("UPDATE employee SET ? WHERE id=?", [req.body, req.params.id], function(err, rows) {
+		if (err) return next(err);
+		selectOne(req.params.id, function(updated) {
+			if (!updated) {
+				res.send(553, {message: 'Update Unsuccessfull'});
+			} else {
+				res.send(updatedRow);
+			}
+		});
+	});
+};
