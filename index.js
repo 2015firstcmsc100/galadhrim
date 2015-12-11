7d24144a0f5e60af421b0f7aba59d5d444897e8a
jgjadaoag <jjadaoag1@up.edu.ph>
2015-12-12 00:30:39 +0800

    Added legolas

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(require('morgan')('dev', {immediate : true}));
 app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
 app.use(require(__dirname + '/config/router')(express.Router(), logger));
+app.use('/arwen', express.static(__dirname + '/arwen'));
+app.use('/elrond', express.static(__dirname + '/elrond'));
+app.use('/galadriel', express.static(__dirname + '/galadriel'));
+app.use('/thranduil', express.static(__dirname + '/thranduil'));
+app.use('/legolas', express.static(__dirname + '/legolas'));
 
 logger.log('info', 'Server listening on port : ', config.port);
 
