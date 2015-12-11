d7dffec7b4eb9e63f94677270b27ca34ce3453c1
Josif78 <seph12.alc@gmail.com>
2015-12-12 02:27:54 +0900

    added angular-route and cookie

--- arwen/index.js
+++ arwen/index.js
@@ -1,16 +1,13 @@
 'use strict';
 
 (function() {
-	angular.module("app", ["ngRoute", "ngCookies"])
+	angular.module("arwen", ["ngRoute", "ngCookies"])
 		.config(config);
 	
 	config.$inject = ["$routeProvider"];
 
 	function config($routeProvider) {
 		$routeProvider
-			.when('/degree-programs', {
-				"controller":"DegreeProgramsCtrl",
-				"templateUrl":"degree-programs/degree-programs.view.html"
-			});
+			
 	}
 })();
