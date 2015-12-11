3b901935834888a77a78fee71ecb338e4280ebce
megatricycle <megatricycle@gmail.com>
2015-12-11 18:55:09 +0800

    placed all FE files on public directory, app now serves static files

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(require(__dirname + '/lib/cors')('*'));
 app.use(require('body-parser')());
 app.use(require('morgan')('dev', {immediate : true}));
 app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
 app.use(require(__dirname + '/config/router')(express.Router(), logger));
 
 logger.log('info', 'Server listening on port : ', config.port);
