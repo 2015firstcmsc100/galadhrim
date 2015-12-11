18136935951de350f74c3cd72a2144138a5560ce
najaafar <nurfitrajaafar@gmail.com>
2015-12-12 00:47:09 +0800

    Update course.js

--- test/course.js
+++ test/course.js
@@ -124,6 +124,51 @@ describe('Course',function(){
 					}
 				});
 		});
+		
+		
+		it('should update a specific course record', function (done) {
+			var update = {
+				'code', 'CMSC100';
+				'name': 'Introduction to Web Programming';
+				'units': '3'
+				'semesterOffered': '1st';
+				'unitId': courseCode;
+			};
+			request(url)
+				.put('/api/courses/' + insertedId)
+				.send(update)
+				.end(function(err, res) {
+					if (err) {
+						throw err;
+					}
+					res.should.have.status(200);
+					res.body.should.have.property('code', 'CMSC100');
+					res.body.should.have.property('name': 'Introduction to Web Programming');
+					res.body.should.have.property('units': '3');
+					res.body.should.have.property('semesterOffered': '1st');
+					res.body.should.have.property('unitId': courseCode);
+					done();
+				});
+		});
+
+
+		it('should return error trying to update a course record that does not exist', function (done) {
+			var update = {
+				'code': randomizedCode + ' (edited)',
+				'name': 'BS Computer Science (edited)'
+			};
+			request(url)
+				.put('/api/courses/0')
+				.send(update)
+				.end(function(err, res) {
+					if (err) {
+					   done();
+					}
+					else {
+					   throw new Error({'message': 'Can retrieve a non-existing course'});
+					}
+				});
+		});
 	});
 		
 });
