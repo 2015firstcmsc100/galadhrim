31a9e4250b357248ceff40db14d56c7769598ce9
rosechellejoy <rosechellejoyoraa@gmail.com>
2015-12-12 08:26:12 +0800

    added ws: retrieve employees

--- gil-galad/controllers/api/employees.js
+++ gil-galad/controllers/api/employees.js
@@ -3,7 +3,7 @@ var logger = require(__dirname + '/../../../lib/logger'),
 
 
 exports.findEmployees = function(req, res, next) {
-	db.query("SELECT * FROM employee", function(err, rows) {
+	db.query("SELECT * FROM employee where _recStatus='ACTIVE'", function(err, rows) {
 		if (err) res.send(400, {message: 'Bad Request.'});
 		else {
 			res.send(200, rows);
