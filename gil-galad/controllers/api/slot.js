86eb4aeb268a3aabce7758d288646e1be4c6c931
santosnarom <santosnarom@gmail.com>
2015-12-12 18:50:37 +0800

    Added WS: Generate Slots

--- gil-galad/controllers/api/slot.js
+++ gil-galad/controllers/api/slot.js
@@ -7,8 +7,8 @@ exports.generateSlots = function(req,res,next){
 
 		for(var i = req.body.numberOfSlots; i > 0; i--){
 				db.query("INSERT INTO slot(sectionId) values(?)",[req.body.sectionId], function(err,rows){
-					if (err) return next(err);
-					return res.send(201, {'success':true, 'message': 'Succesful slot generation'});
+					if (err) return next();
 				});
+				if(i == 1) res.send(200, {message: 'Slot generation successful'});
 		}
 }
