9a817037079263a1b409b2ab3fd729cdb55b1a15
Matthew <matthewmarcos94@gmail.com>
2015-12-12 09:47:57 +0800

    Created test for post request to rooms

--- test/room.js
+++ test/room.js
@@ -8,6 +8,29 @@ describe('room', function() {
 	var url = 'http://galadhrim.loc:' + (process.env.PORT || config.port);
 	var roomId = 0;
 
+// START
+	describe('Post request to room', function() {
+		it('should create a new room', function (done) {
+			var newRoom = {
+				room: "Test room";
+			};
+			request(url)
+				.post('/api/rooms')
+				.send(newRoom)
+				.end(function(err, res) {
+					if (err) {
+						throw err;
+					}
+					else {
+						res.should.have.status(200);
+						res.body.should.have.property('room', 'Test Room');
+						done();
+					}
+				});
+		});
+	});
+// END
+
 	describe('update()', function () {
 		
 		it('should update a specific room: name field only', function (done) {
