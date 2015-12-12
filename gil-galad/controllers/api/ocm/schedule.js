d6718dcec6125a943f56decf642d7d849fb78570
jdtdelrio <judedelrio@gmail.com>
2015-12-12 10:34:31 +0800

    Added Retrieve schedule of student(updated)

--- gil-galad/controllers/api/ocm/schedule.js
+++ gil-galad/controllers/api/ocm/schedule.js
@@ -2,7 +2,7 @@ var logger = require(__dirname + '/../../../../lib/logger'),
     db = require(__dirname + '/../../../lib/mysql');
 
 exports.find = function(req, res, next) {
-	db.query("select * from slot where status = ("ENLISTED" or "RESERVED") and studentId=?", [req.params.id], function(err, rows) {
+	db.query("select * from slot where status like ("ENLISTED" or "RESERVED") and studentId=?", [req.params.id], function(err, rows) {
 		if (err) return next(err);
 		if (rows.length === 0) {
 			res.send(404, {message: 'Schedule not found.'});
