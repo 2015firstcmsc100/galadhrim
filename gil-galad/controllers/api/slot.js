ddddc79089975336459dcf5f0bb4523dcc244d06
santosnarom <santosnarom@gmail.com>
2015-12-12 18:20:16 +0800

    Added WS: Generate Slots

--- gil-galad/controllers/api/slot.js
+++ gil-galad/controllers/api/slot.js
@@ -2,11 +2,11 @@ var	logger = require(__dirname + '/../../../lib/logger'),
 	db = require(__dirname + '/../../lib/mysql');
 
 exports.generateSlots = function(req,res,next){
-		if(req.params.sectionId == undefined || req.params.numberOfSlots == undefined)
+		if(req.body.sectionId == undefined || req.body.numberOfSlots == undefined)
 			return res.send(451, {'error': true, 'message': 'Missing parameter'});
 
-		for(var i = req.params.numberOfSlots; i > 0; i--){
-				db.query("INSERT INTO slot(sectionId) values(?)",[req.params.sectionId], function(err,next){
+		for(var i = req.body.numberOfSlots; i > 0; i--){
+				db.query("INSERT INTO slot(sectionId) values(?)",[req.body.sectionId], function(err,next){
 					if (err) return next(err);
 					return res.send(201, {'success':true, 'message': 'Succesful slot generation'});
 				});
