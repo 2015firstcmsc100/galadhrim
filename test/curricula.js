d01c75e959482447872e9460a245b9c9ff151028
jancrissel <joderamos@up.edu.ph>
2015-12-13 20:49:16 +0800

    Added test case suite for UPDATE /api/curricula

--- test/curricula.js
+++ test/curricula.js
@@ -19,3 +19,73 @@ describe ('Retrieving all Curricula', function() {
             });
     });
 });
+
+//Add test case suite for UPDATE /api/curricula
+describe('Update Curriculum',function(){	
+
+	describe("update()", function(){
+	
+		it('should update a specific curriculum: code field only', function (done) {
+			var update = {
+				'code': 'BSCS-SP-2013-12-40';
+			};
+			request(url)
+				.put('/api/curricula/' + insertedId)
+				.send(update)
+				.end(function(err, res) {
+					if (err) {
+						throw err;
+					}
+					else {
+						res.should.have.status(200);
+						res.body.should.have.property('code': 'BSCS-SP-2013-12-40');
+						res.body.should.have.property('name': 'Sample');
+						res.body.should.have.property('degreeProgramId': '3');
+						done();
+					}
+				});
+		});
+		
+		it('should update a specific curriculum: name field only', function (done) {
+			var update = {
+				'name': 'Sample';
+			};
+			request(url)
+				.put('/api/curricula/' + insertedId)
+				.send(update)
+				.end(function(err, res) {
+					if (err) {
+						throw err;
+					}
+					else {
+						res.should.have.status(200);
+						res.body.should.have.property('code': 'BSCS-SP-2013-12-40');
+						res.body.should.have.property('name': 'Sample');
+						res.body.should.have.property('degreeProgramId': '3');
+						done();
+					}
+				});
+		});
+		
+		it('should update a specific curriculum: degree program id field only', function (done) {
+			var update = {
+				'degreeProgramId': '3';
+			};
+			request(url)
+				.put('/api/curricula/' + insertedId)
+				.send(update)
+				.end(function(err, res) {
+					if (err) {
+						throw err;
+					}
+					else {
+						res.should.have.status(200);
+						res.body.should.have.property('code': 'BSCS-SP-2013-12-40');
+						res.body.should.have.property('name': 'Sample');
+						res.body.should.have.property('degreeProgramId': '3');
+						done();
+					}
+				});
+		});
+	});
+});
